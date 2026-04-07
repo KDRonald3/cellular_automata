01bcd9b4b789da80447d3c4effd570b00250f8fe
KDRonald3 <kouatchouadjakouronald@gmail.com>
2026-04-07 15:20:29 -0700

    Added implementations for the rule structure. this lets the user decide which rule they want to run

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,12 @@ use std::io::stdin;
 // use std::process::Output;
 
 const ONE: i32 = 1;
+const ZERO: i32 = 0;
 
 struct Structure {
     structure: HashMap<i32, Vec<i32>>,
 }
 
-// struct Error {
-//     veracity: bool,
-//     err: String,
-// }
 
 impl Structure {
     fn get_slice(&self, generation: &i32, center: &i32) -> Result<&Vec<i32>, String> {
@@ -29,9 +26,34 @@ impl Structure {
 }
 
 struct Rule {
+    number: i32,
     rule: HashMap<Vec<i32>, i32>,
 }
 
+impl Rule {
+    fn base_10_to_2(&self, number_base_10: &i32) -> Vec<i32>{
+        let mut number = *number_base_10
+        let mut number_base_2 = Vec::new();
+        
+        while number != 0{
+            number_base_2.insert(0,number%2);
+            number = (number - number%2)/2;
+        };
+        while number_base_2.len() < 8{
+            number_base_2.insert(0,0);
+        }
+
+        return number_base_2;
+    }
+
+    fn create_rule(& mut self, rule_number:& mut i32){
+        self.number = *rule_number;
+        let rule_number_base_2 = self.base_10_to_2(&self.number);
+        for i in 0..8{
+            self.rule.insert(self.base_10_to_2(&i),rule_number_base_2[i as usize]);
+        }
+    }
+}
 struct InitialCondition {
     size: usize,
     state: Vec<i32>,
@@ -59,40 +81,6 @@ impl CellularAutomaton {
         initial_condition.state.push(ONE);
         structure.structure.insert(0, initial_condition.state);
 
-        // for generation in 0..final_generation {
-        //     for thread in 0..number_of_threads {
-        //         let slice: &[i32];
-
-        //         match structure.structure.get(&generation) {
-        //             Some(generation_state) => slice = &generation_state[thread - 1..thread + 1],
-        //             None => println!(
-        //                 "could not find generation number in automaton map{}",
-        //                 generation - 1
-        //             ),
-        //         }
-
-        //         let rule_output = match rule.rule.get(slice) {
-        //             Some(value) => value,
-        //             None => &9,
-        //         };
-
-        //         match structure.structure.get_mut(&(generation + 1)) {
-        //             Some(generation_state) => {
-        //                 if generation_state.len() < thread {
-        //                     generation_state.push(*rule_output);
-        //                 } else {
-        //                     generation_state.insert(thread, *rule_output)
-        //                 }
-        //             }
-        //             None => {
-        //                 structure
-        //                     .structure
-        //                     .insert(generation + 1, vec![*rule_output]);
-        //             }
-        //         }
-        //     }
-        // }
-
         for generation in 0..final_generation {
             structure.structure.insert(
                 generation + 1,
@@ -119,6 +107,7 @@ impl CellularAutomaton {
         self.structure = structure;
     }
 }
+
 fn main() {
     let mut initial_state_string = String::new();
     let mut initial_state_vec: Vec<i32> = Vec::new();
@@ -126,6 +115,8 @@ fn main() {
         size: (0),
         state: (Vec::new()),
     };
+
+    // let cellular_automaton: CellularAutomaton = CellularAutomaton {};
 
     println!("Hello, world!");
 
@@ -138,7 +129,7 @@ fn main() {
         match state.to_digit(10) {
             Some(n) => {
                 initial_state_vec.push(n as i32);
-                println!("{}",n);
+                println!("{}", n);
             }
             None => continue,
         };
